579ed1431b215a143cc947e6385682ccccca8f6b
Ondřej Kupka <ondra.cap@gmail.com>
2017-07-23 23:27:22 +0200

    Bootstrap Material

---
+++ src/app/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { MdButtonModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    MdButtonModule
+  ],
+  exports: [
+    MdButtonModule
+  ]
+})
+export class MaterialModule { }
